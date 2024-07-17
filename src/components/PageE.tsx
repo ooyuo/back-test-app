079f3c1f64eee9af622adb545a3f9e0af95683bf
ooyuo <doyounjeong0@gmail.com>
2024-07-17 22:33:18 +0900

    fix: fix syntax for build

--- src/components/PageE.tsx
+++ src/components/PageE.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 function PageE() {
   return <div>PageE</div>;
 }
--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,4 @@ export default defineConfig({
   build: {
     outDir: "dist",
   },
-  server: {
-    historyApiFallback: true,
-  },
 });
