a91d36992d6be28718aa3d29d3e526cac6784e3f
ooyuo <doyounjeong0@gmail.com>
2024-08-27 23:58:40 +0900

    test

--- src/App.tsx
+++ src/App.tsx
@@ -8,22 +8,35 @@ function App() {
   useEffect(() => {
     if (isMobile) {
       // 모바일 장치에서만 동작하도록 설정
-      const rootElement = document.getElementById("root");
-      if (rootElement) {
-        const adjustHeight = () => {
-          rootElement.style.height = `${window.innerHeight}px`;
-        };
+      const rootElement = document.getElementById("root") as HTMLElement;
+      const bodyElement = document.body as HTMLElement;
+      const infoFixElement = document.querySelector(".info-fix") as HTMLElement;
 
-        // 초기 높이 설정
-        adjustHeight();
+      const adjustHeight = () => {
+        const viewportHeight = window.innerHeight;
 
-        // 뷰포트 크기가 변경될 때마다 높이 조정
-        window.addEventListener("resize", adjustHeight);
+        if (rootElement) {
+          rootElement.style.height = `${viewportHeight}px`;
+        }
 
-        return () => {
-          window.removeEventListener("resize", adjustHeight);
-        };
-      }
+        if (bodyElement) {
+          bodyElement.style.height = `${viewportHeight}px`;
+        }
+
+        if (infoFixElement) {
+          infoFixElement.style.height = `${viewportHeight - 30}px`;
+        }
+      };
+
+      // 초기 높이 설정
+      adjustHeight();
+
+      // 뷰포트 크기가 변경될 때마다 높이 조정
+      window.addEventListener("resize", adjustHeight);
+
+      return () => {
+        window.removeEventListener("resize", adjustHeight);
+      };
     }
   }, []);
 
